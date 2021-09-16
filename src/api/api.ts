c84805d46117ed92185f34e99355f249076c6fc5
Zahid Ronty <230759@usn.no>
2021-09-16 22:25:02 +0200

    second commit

---
+++ src/api/api.ts
@@ -0,0 +1,34 @@
+import axios, {AxiosResponse} from 'axios';
+import {IProduct, UserCartType} from '../interfaces'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 15000,
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+	get: (url: string) => instance.get(url).then(responseBody),
+  delete: (url: string) => instance.delete(url).then(responseBody),
+  put: (url: string, body: {}) => instance.put(url, body).then(responseBody),
+  post: (url: string, body: {}) => instance.post(url, body).then(responseBody),
+};
+
+
+export const Get={
+  getProducts: (): Promise<IProduct[]> => requests.get('products'),
+  getUserCart: (id:number): Promise<UserCartType> => requests.get('carts/'+ id),
+  getProduct: (id:number): Promise<IProduct> => requests.get('products/'+ id),
+  searchProduct: (query:string): Promise<IProduct[]> => requests.get('products?q='+ query),
+}
+
+export const Delete={
+  deleteProduct: (id:number): Promise<void> => requests.delete('products/'+id)
+}
+
+export const Put={
+  updateProduct: (post: IProduct, id: number): Promise<IProduct> =>requests.put('/products/' + id, post)
+}
+export const Post={
+  addProduct: (post: IProduct): Promise<IProduct> =>requests.post('/products', post)}
---
+++ src/components/EditModal/EditModal.tsx
@@ -0,0 +1,83 @@
+import React,{useContext, useEffect, useState} from 'react'
+import styles from './editModal.module.css'
+import {MdCancel} from 'react-icons/md'
+//context
+import CommonContext from '../../context/common'
+//api
+import {Put} from '../../api/api'
+
+const EditModal: React.FunctionComponent = () => {  
+    const commonContext = useContext(CommonContext)
+    const updateEdit = commonContext.updateIsEdit
+    const edit = commonContext.isEdit
+    const updateIsEdit = commonContext.updateIsEdit
+    const updateIsLoading = commonContext.updateIsLoading
+    const selectedProduct = commonContext.selectedProduct
+    const updateProducts = commonContext.getAllProducts
+
+    const [newValue, setNewValue] = useState<number|string>()
+
+    useEffect(()=>{
+      if(edit==='name'){setNewValue(selectedProduct.name)}
+      else if(edit==='description'){setNewValue(selectedProduct.description)}
+      else if(edit==='price'){setNewValue((selectedProduct.price))}
+      else if(edit==='discount'){setNewValue(selectedProduct.discount)}
+    },[edit])
+
+
+    const uploadImage =(event:any)=>{
+      let file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        if(typeof(reader.result)==='string'){setNewValue(reader.result)}
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    }
+    
+    const save = ()=>{
+      updateIsLoading(true)
+      if(edit==='name' && typeof(newValue)==='string'){
+        console.log(edit)
+        selectedProduct.name = newValue
+        Put.updateProduct(selectedProduct, selectedProduct.id)
+      }else if(edit==='description' && typeof(newValue)==='string'){
+        console.log(edit)
+        selectedProduct.description = newValue
+        Put.updateProduct(selectedProduct, selectedProduct.id)
+      }else if(edit==='price' && typeof(newValue)==='number'){
+        console.log(edit)
+        selectedProduct.price = newValue
+        Put.updateProduct(selectedProduct, selectedProduct.id)
+      }else if(edit==='discount' && typeof(newValue)==='number'){
+        console.log(edit)
+        selectedProduct.discount = newValue
+        Put.updateProduct(selectedProduct, selectedProduct.id)
+      }else if(edit==='image' && typeof(newValue)==='string'){
+        console.log(edit)
+        selectedProduct.defaultImage = newValue
+        Put.updateProduct(selectedProduct, selectedProduct.id)
+      }
+      updateProducts()
+      updateIsEdit('')
+      updateIsLoading(false)
+      setNewValue('') 
+    }
+
+    return( 
+      <section className={styles.container}>
+        <div className={styles.modal}>
+          <MdCancel className={styles.icon} onClick={()=>updateEdit('')}/>
+          <h2>Edit the {edit} </h2>
+          { (edit==='name'||edit==='description') && <input type="text" onChange={(e)=>setNewValue(e.target.value)}  value={newValue}  />}
+          { (edit==='price'||edit==='discount') && <input type="number" step="any" onChange={(e)=>setNewValue(parseInt(e.target.value))}  value={newValue}  />}
+          { (edit==='image') && <input type="file"  onChange={(e)=>uploadImage(e)}  accept=".jpeg, .png, .jpg" />}
+          <button type="button" onClick={()=>save()} >Save</button>
+        </div>
+      </section>
+    )
+  }
+
+export default EditModal;
